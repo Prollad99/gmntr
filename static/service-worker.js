c1946574dde268ca50aa487594bb2e52c5a93fd4
Prollad Mondal <prolladmail@gmail.com>
2024-07-24 16:00:42 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,8 @@
 self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
   event.waitUntil(
     caches.open('static-cache').then(function(cache) {
+      console.log('Caching app shell');
       return cache.addAll([
         '/',
         '/index.html',
@@ -14,9 +16,17 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request);
+    }).catch(function(error) {
+      console.error('Error fetching and caching new data', error);
     })
   );
 });
