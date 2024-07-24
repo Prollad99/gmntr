52985b073ebf500db1687f77e4c487499cab72ff
Prollad Mondal <prolladmail@gmail.com>
2024-07-24 16:40:22 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,22 +1,7 @@
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('static-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/main.css',
-        '/manifest.json',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
-      ]);
-    })
-  );
+  console.log('Service worker installed');
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  console.log('Fetch event for ', event.request.url);
 });
