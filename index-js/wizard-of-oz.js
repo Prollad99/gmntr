4a494ee0313afa808d44b622d97fc80239122aaa
Prollad Mondal <prolladmail@gmail.com>
2024-07-24 22:19:46 +0530

    Update wizard-of-oz.js

--- index-js/wizard-of-oz.js
+++ index-js/wizard-of-oz.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
@@ -8,9 +8,14 @@ const url = 'https://www.facebook.com/SlotsWizardOfOz/';
 const maxLinks = 100;
 const currentDate = moment().format('YYYY-MM-DD');
 
-axios.get(url)
-  .then(({ data }) => {
-    const $ = cheerio.load(data);
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    const content = await page.content();
+    const $ = cheerio.load(content);
     const links = [];
 
     $('a[href*="zdnwoz0-a.akamaihd.net"], a[href*="zynga.social"]').each((index, element) => {
@@ -23,15 +28,17 @@ axios.get(url)
     console.log('Fetched links:', links);
 
     const dir = 'links-json';
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
 
     const filePath = path.join(dir, 'wizard-of-oz.json');
     fs.writeFileSync(filePath, JSON.stringify(links, null, 2), 'utf8');
     console.log(`Links saved to ${filePath}`);
-  })
-  .catch(err => {
+
+    await browser.close();
+  } catch (err) {
     console.error('Error fetching links:', err);
     process.exit(1);
-  });
+  }
+})();
