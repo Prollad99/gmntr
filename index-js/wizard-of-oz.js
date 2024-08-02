f330ac8501a906be7434ac43e23a2ce2319c6c03
Prollad Mondal <prolladmail@gmail.com>
2024-08-02 21:36:09 +0530

    Update wizard-of-oz.js

--- index-js/wizard-of-oz.js
+++ index-js/wizard-of-oz.js
@@ -35,7 +35,7 @@ axios.get(url)
       newLinks.push({ href: link, text: "WOZ Free Coins", date: date });
     });
 
-    // Remove duplicates and limit the list to 100 links
+    // Combine new links with existing links, keeping the older dates if they exist
     const combinedLinks = [...newLinks, ...existingLinks]
       .reduce((acc, link) => {
         if (!acc.find(({ href }) => href === link.href)) {
