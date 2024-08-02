c41ec229d285a2e5ef3f487e708c59179eb7d64a
Prollad Mondal <prolladmail@gmail.com>
2024-08-02 18:57:38 +0530

    Update wizard-of-oz.js

--- index-js/wizard-of-oz.js
+++ index-js/wizard-of-oz.js
@@ -3,17 +3,26 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 
+// Function to get the current date in YYYY-MM-DD format
+function getCurrentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
 const url = 'https://mosttechs.com/wizard-of-oz-slots-free-coins/';
 
 axios.get(url)
   .then(({ data }) => {
     const $ = cheerio.load(data);
     const links = [];
+    const currentDate = getCurrentDate();
 
     $('a[href*="zdnwoz0-a.akamaihd.net"], a[href*="zynga.social"]').each((index, element) => {
       const link = $(element).attr('href');
-      const text = $(element).text().trim();
-      links.push({ href: link, text: text });
+      links.push({ href: link, date: currentDate });
     });
 
     console.log('Fetched links:', links);
